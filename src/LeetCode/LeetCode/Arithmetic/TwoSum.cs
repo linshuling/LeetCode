81c4ba3be1479dcf4f81035104bedfb4b9eab79f
dabao <403331581@qq.com>
2016-03-07 20:33:12 +0800

    The First Arithmetic，Go on March 7th, 2016 Signed-off-by: dabao <403331581@qq.com>

---
+++ src/LeetCode/LeetCode/Arithmetic/TwoSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace LeetCode.Arithmetic
+{
+    public class TwoSum
+    {
+        public int[] Solution(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    result[0] = dic[nums[i]];
+                    result[1] = i;
+                    return result;
+                }
+
+                //Choose First Index
+                if (!dic.ContainsKey(target - nums[i]))
+                    //The Key is the Second Number
+                    dic.Add(target - nums[i], i);
+            }
+
+            throw new Exception("Numbers Do Not Exist");
+        }
+    }
+}
---
+++ src/LeetCode/LeetCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Arithmetic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSum two = new TwoSum();
+
+            int[] first = new int[] { 9, 5, 2, 1, 8 };
+            int target = 17;
+            int[] result = two.Solution(first, target);
+
+            Console.WriteLine("First Index:{0},Second Index:{1}", result[0], result[1]);
+
+            int[] second = new int[] { -4, -5, 0, 0, 18, 200, 5, 0 };
+            target = 11111;
+            result = two.Solution(second, target);
+
+            Console.WriteLine("First Index:{0},Second Index:{1}", result[0], result[1]);
+            Console.ReadKey();
+        }
+    }
+}
