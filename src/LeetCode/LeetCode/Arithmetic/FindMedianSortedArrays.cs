6eeb93b073455ba1fd5d27f792e32fa57e53af7d
dabao <403331581@qq.com>
2016-03-09 20:49:34 +0800

    Didn't do 3th, Oh no! March 9th,2016 Signed-off-by: dabao <403331581@qq.com>

---
+++ src/LeetCode/LeetCode/Arithmetic/FindMedianSortedArrays.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Arithmetic
+{
+    public class FindMedianSortedArrays
+    {
+        //O((m+n)/2)
+        public double Solution(int[] nums1, int[] nums2)
+        {
+            int len1 = nums1.Length;
+            int len2 = nums2.Length;
+            int middle = (len1 + len2) / 2;
+            int[] newnums = new int[middle + 1];
+            int index = 0;
+            int i = 0;
+            int j = 0;
+            while (index <= middle)
+            {
+                if (i < len1 && j < len2)
+                {
+                    if (nums1[i] <= nums2[j])
+                    {
+                        newnums[index] = nums1[i];
+                        i++;
+                    }
+                    else
+                    {
+                        newnums[index] = nums2[j];
+                        j++;
+                    }
+                }
+                else if (i == len1)
+                {
+                    newnums[index] = nums2[j];
+                    j++;
+                }
+                else
+                {
+                    newnums[index] = nums1[i];
+                    i++;
+                }
+
+                index++;
+            }
+
+            double result = 0;
+            if ((len1 + len2) % 2 == 0)
+            {
+                result = (newnums[middle] + newnums[middle - 1]) * 0.5;
+            }
+            else
+            {
+                result = newnums[middle];
+            }
+            return result;
+        }
+    }
+}
---
+++ src/LeetCode/LeetCode/Arithmetic/LengthOfLongestSubstring.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace LeetCode.Arithmetic
+{
+    public class LengthOfLongestSubstring
+    {
+        //Let me think
+        public int Solution(string s)
+        {
+            //char[] chars = s.ToCharArray();
+
+            //Hashtable hash = new Hashtable();
+
+            //int longest = 0;
+
+            //for (int i = 0; i < s.Length;i++ )
+            //{
+            //    if(hash.ContainsKey(chars[i]))
+            //    {
+            //        longest = longest >= i - (int)hash[chars[i]] ? longest : i - (int)hash[chars[i]];
+
+            //        hash[chars[i]] = i;
+            //    }
+            //    else
+            //    {
+            //        hash.Add(chars[i], i);
+            //    }
+            //}
+
+            //return longest;
+        }
+    }
+}
